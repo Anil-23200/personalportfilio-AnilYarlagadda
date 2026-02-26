32eb3319e7cc64010fc3e11c01d6750403e3843e
Anil-23200 <anilyarlagadda6@gmail.com>
2026-02-26 12:50:19 +0530

    added email service

---
+++ script.js
@@ -0,0 +1,27 @@
+function contactMe(event) {
+    event.preventDefault(); 
+    emailjs.init({
+        publicKey: '-e3iVlZmkErzJUW8P',
+    });
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    console.log("Name:", name)
+
+    const info = {
+        form_name: name,
+        form_email: email,
+        text: message
+    };
+
+    emailjs.send('service_31vvnof', 'template_v1jfss2', info)
+        .then(function () {
+            alert("Email Sent Successfully!");
+        })
+        .catch(function (error) {
+            console.log(error);
+            alert("Email Not Sent!");
+        });
+}
